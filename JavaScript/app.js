0ad1e800ae19ec17ef7205438f977f51232420eb
4mpl1f13d <jlpeters@gmail.com>
2020-03-19 10:08:09 -0500

    Added CSS to HTML site. Also added JS playground for lectures

---
+++ JavaScript/app.js
@@ -0,0 +1,3 @@
+var message = prompt ("Please enter your message... ");
+// var remaining = (280 - message.length);
+alert("You have written " +message.length+ " characters. You have " +(280 - message.length)+ " characters remaining.");
