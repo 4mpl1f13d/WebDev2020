edf0ecfee6d2bc798a1f31ac4e92d8ee4291e40b
4mpl1f13d <jlpeters@gmail.com>
2020-04-09 16:09:27 -0500

    added NodeJS

---
+++ NodeJS/index.js
@@ -0,0 +1,30 @@
+// -- First Node JS command without using Web Browser
+// console.log("Web Dev 2020 - Hello World!");
+
+// -- REPL (Read Evaluation Print Loop) execute code in chunks using NODE... 
+
+
+// -- EX 1 - Using Native Node Modules
+//jshint esversion:6
+// const fs=require('fs');
+// -- created file1.txt (I am file 1.)
+// -- run command below to copy to file2.txt(I am file 2.)
+// fs.copyFileSync("file1.txt","file2.txt");
+// -- overwrites any values present in that file. 
+
+
+// -- EX 2 - Using Native Node Modules
+// -- install npm superheroes
+// var superheroes = require('superheroes');
+// var mySuperHeroName = superheroes.random();
+// console.log(mySuperHeroName);
+
+
+
+
+
+
+
+
+
+
