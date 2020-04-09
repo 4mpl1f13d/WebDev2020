601c7dd5565894297d436f087c88a2e8ef145e88
4mpl1f13d <jlpeters@gmail.com>
2020-04-09 16:20:41 -0500

    added NodeJS ... finished

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,10 +1,10 @@
-// -- First Node JS command without using Web Browser
+// // -- First Node JS command without using Web Browser
 // console.log("Web Dev 2020 - Hello World!");
 
-// -- REPL (Read Evaluation Print Loop) execute code in chunks using NODE... 
+// // -- REPL (Read Evaluation Print Loop) execute code in chunks using NODE... 
+// node setup, and node index.js execution...
 
-
-// -- EX 1 - Using Native Node Modules
+// // -- EX 1 - Using Native Node Modules
 //jshint esversion:6
 // const fs=require('fs');
 // -- created file1.txt (I am file 1.)
@@ -12,12 +12,21 @@
 // fs.copyFileSync("file1.txt","file2.txt");
 // -- overwrites any values present in that file. 
 
-
-// -- EX 2 - Using Native Node Modules
-// -- install npm superheroes
+// // -- EX 2 - Using Native Node Modules
+// // -- install npm superheroes
 // var superheroes = require('superheroes');
 // var mySuperHeroName = superheroes.random();
 // console.log(mySuperHeroName);
+
+// // -- EX 2 & 3 can be used in tandem
+
+// // -- EX 3 - Using Native Node Modules
+// // -- install npm supervillains
+// var supervillains = require('supervillains');
+// var mySuperVillainName = supervillains.random();
+// console.log(mySuperVillainName);
+
+
 
 
 
